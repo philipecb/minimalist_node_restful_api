6f3ad7e92ce85cc20c03214c63db38a7f26d02c3
philipecb <philipe.cb97@hotmail.com>
2019-03-07 17:00:15 -0300

    Update on index.js, with PORT information.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ let express = require('express')
 let app = express()
 
 app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+app.list(PORT, () => console.info('Server has started on ${PORT}'))
